d8f310f1648d0bf53e68545560b8205783b0a68a
Mom-Dra <yem9087@gmail.com>
2024-07-09 18:03:49 +0900

    Init

---
+++ SnakeGame/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <Windows.h>
+
+// INPUT
+// 입력 받기
+// 
+// UPDATE
+// 입력에 따른 로직 세팅
+// 입력에 따른 버퍼 세팅!
+// 
+// RENDER
+// 출력
+
+HANDLE g_hScreen[2];
+
+int main()
+{
+	int a;
+
+	CONSOLE_CURSOR_INFO cci;
+
+	// 화면 버퍼 2개를 만든다
+	
+
+}
